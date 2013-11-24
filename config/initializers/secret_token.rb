72c296696e11e088850c27ffd8b2f33b13423644
Doug Ferreira <dukecitykid@gmail.com>
2013-11-24 14:35:10 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pawdl::Application.config.secret_key_base = 'c0f57228609d6cbf45bd937b8a07ed93bce4b98742b89418640ab6d78f9111a1f04e44395a9adc43c0556c7bc99bfe76246f33adea6ae65210262831a4a6be5e'
